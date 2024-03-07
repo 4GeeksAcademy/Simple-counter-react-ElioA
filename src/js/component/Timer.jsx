20c65e603faada0aef8d535dce527abd1a7a6710
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-03-07 19:25:14 +0000

    my second commit

--- src/js/component/Timer.jsx
+++ src/js/component/Timer.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect, useRef} from "react";
-import Forms from "./form";
 import Button from "./buttons";
 
 const Timer=()=>{
@@ -22,29 +21,37 @@ const Timer=()=>{
     const [startingV, setStratingV]= useState("")
     const [alertV, setAlertV]=useState("")
         
-    if(alertV==counter && alertV!=""){alert("hello")}
-     if(startingV==""){}
+    if(alertV==counter && alertV!=""){alert("we got it")}
+  
+  
+console.log(counter)
 
 return <div> 
     <div className="bigCounter">
              <div className="clock"><i class="far fa-clock"></i></div>
-            <div className="fourthD">{Math.floor(counter/1000) % 10}</div>
-            <div className="thirdD">{Math.floor(counter/100) % 10}</div>
-            <div className="secondD">{Math.floor(counter/10) % 10}</div>
-            <div className="firstD">{counter % 10}</div>
+             <div>{(Math.floor(counter/1000000)) % 10}</div>
+             <div>{(Math.floor(counter/100000)) % 10}</div>
+             <div>{(Math.floor(counter/10000)) % 10}</div>
+            <div>{(Math.floor(counter/1000)) % 10}</div>
+            <div>{(Math.floor(counter/100))% 10}</div>
+            <div>{(Math.floor(counter/10)) % 10}</div>
+            <div>{counter%10}</div>
     </div>
     <div className="d-flex justify-content-center p-3">
-            <Button name="Stop" color="red" event={()=>clearInterval(id.current)}></Button>
-			<Button name="Start" color="green" event={()=>handelTime()}></Button>
-			<Button name="Reset" color="blue" event={()=>{
+          <Button name="Stop" color="red" event={()=>clearInterval(id.current)}></Button>
+			    <Button name="Start" color="green" event={()=>handelTime()}></Button>
+			    <Button name="Reset" color="blue" event={()=>{
                 clearInterval(id.current)
                 setCounter(0)
                 handelTime()}} ></Button>
     </div>
     <div className="container d-flex justify-content-center">
-    <input type="text" className="form-control"  placeholder="Set Alert Here" aria-label="StartingValue" value={alertV} onChange={(e)=> setAlertV(e.target.value)}></input>
-    <input type="text" className="form-control"  placeholder="Set Starting Value Here" aria-label="StartingValue" value={startingV} onChange={(e)=> setStratingV(e.target.value)}></input>          
-    </div>
+        <input type="text" className="form-control"  placeholder="Set Alert Here" aria-label="StartingValue" value={alertV} onChange={(e)=> setAlertV(e.target.value)}></input> 
+        <input type="text" className="form-control"  placeholder="Set Starting Value Here" aria-label="StartingValue" value={startingV} onChange={(e)=> setStratingV(Number(e.target.value))}></input> 
+  </div>
+           <div className="container d-flex justify-content-center p-3">
+            <Button color="yellow" name="Press to set Starting Value" textcolor="black" event={()=>setCounter(startingV)}/>
+           </div>
   </div>
       
 
@@ -53,4 +60,4 @@ return <div>
 
 
 
-export default Timer;
+export default Timer
--- src/js/component/buttons.jsx
+++ src/js/component/buttons.jsx
@@ -3,10 +3,10 @@ import React, {useState, useEffect} from "react";
 
 
 
-const Button=({color, name, event})=>{
+const Button=({color, name, event, textcolor})=>{
 
     
-    return   <button type="button" className="btn btn-primary btn-lg mx-3 border-0" style={{backgroundColor: color}} onClick={event}>{name}</button>
+    return   <button type="button" className="btn btn-primary btn-lg mx-3 border-0" style={{backgroundColor: color, color:textcolor}} onClick={event}>{name}</button>
 
 
 }
