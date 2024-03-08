642330262c762c16b199050d8d775b522fa008b5
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-03-08 18:18:38 +0000

    last commit

--- src/js/component/Timer.jsx
+++ src/js/component/Timer.jsx
@@ -26,33 +26,35 @@ const Timer=()=>{
   
 console.log(counter)
 
-return <div> 
+return( 
+<div> 
     <div className="bigCounter">
-             <div className="clock"><i class="far fa-clock"></i></div>
-             <div>{(Math.floor(counter/1000000)) % 10}</div>
-             <div>{(Math.floor(counter/100000)) % 10}</div>
-             <div>{(Math.floor(counter/10000)) % 10}</div>
+            <div className="clock"><i class="far fa-clock"></i></div>
+            <div>{(Math.floor(counter/1000000)) % 10}</div>
+            <div>{(Math.floor(counter/100000)) % 10}</div>
+            <div>{(Math.floor(counter/10000)) % 10}</div>
             <div>{(Math.floor(counter/1000)) % 10}</div>
             <div>{(Math.floor(counter/100))% 10}</div>
             <div>{(Math.floor(counter/10)) % 10}</div>
             <div>{counter%10}</div>
     </div>
     <div className="d-flex justify-content-center p-3">
-          <Button name="Stop" color="red" event={()=>clearInterval(id.current)}></Button>
-			    <Button name="Start" color="green" event={()=>handelTime()}></Button>
-			    <Button name="Reset" color="blue" event={()=>{
+             <Button name="Stop" color="red" event={()=>clearInterval(id.current)}></Button>
+		     <Button name="Start" color="green" event={()=>handelTime()}></Button>
+		     <Button name="Reset" color="blue" event={()=>{
                 clearInterval(id.current)
                 setCounter(0)
-                handelTime()}} ></Button>
+                handelTime()}} >
+             </Button>
     </div>
     <div className="container d-flex justify-content-center">
         <input type="text" className="form-control"  placeholder="Set Alert Here" aria-label="StartingValue" value={alertV} onChange={(e)=> setAlertV(e.target.value)}></input> 
         <input type="text" className="form-control"  placeholder="Set Starting Value Here" aria-label="StartingValue" value={startingV} onChange={(e)=> setStratingV(Number(e.target.value))}></input> 
-  </div>
-           <div className="container d-flex justify-content-center p-3">
+    </div>
+    <div className="container d-flex justify-content-center p-3">
             <Button color="yellow" name="Press to set Starting Value" textcolor="black" event={()=>setCounter(startingV)}/>
-           </div>
-  </div>
+    </div>
+</div>)
       
 
 }
--- src/js/component/buttons.jsx
+++ src/js/component/buttons.jsx
@@ -6,7 +6,7 @@ import React, {useState, useEffect} from "react";
 const Button=({color, name, event, textcolor})=>{
 
     
-    return   <button type="button" className="btn btn-primary btn-lg mx-3 border-0" style={{backgroundColor: color, color:textcolor}} onClick={event}>{name}</button>
+    return  (<button type="button" className="btn btn-primary btn-lg mx-3 border-0" style={{backgroundColor: color, color:textcolor}} onClick={event}>{name}</button>)
 
 
 }
